5d9054bde77df9f43438ea02a3be286be186f8e8
HermanPlay <hermanplay1110@gmail.com>
2024-12-08 17:13:18 +0100

    day 2 linear

--- solutions/day2/solution.go
+++ solutions/day2/solution.go
@@ -1,6 +1,7 @@
 package day2
 
 import (
+	"fmt"
 	"log"
 	"math"
 	"strconv"
@@ -22,11 +23,9 @@ func verify(a, b string, increasing, decreasing bool) bool {
 
 	// Verify difference is in range [1, 3]
 	if diffAbs < 1 || diffAbs > 3 {
-		log.Printf("Diff is out of range (%d %d)", last, num)
 		return false
 	}
 	if (increasing && diff <= 0) || (decreasing && diff >= 0) {
-		log.Printf("Not increaing nor decreasing (%d %d)", last, num)
 		return false
 	}
 
@@ -111,10 +110,10 @@ func Part1(input string) int {
 			last = num
 		}
 		if correct {
-			log.Printf("Report %s, is safe\n", report)
+			fmt.Printf("Report %s, is safe\n", report)
 			safe += 1
 		} else {
-			log.Printf("Report %s, is not safe\n", report)
+			fmt.Printf("Report %s, is not safe\n", report)
 		}
 
 		// reader := bufio.NewReader(os.Stdin)
@@ -124,7 +123,80 @@ func Part1(input string) int {
 	return safe
 }
 
+func isSafe(report []string, excludeIdx int) bool {
+	var last int64 = -1
+	increasing, decreasing := false, false
+	for i := 0; i < len(report); i++ {
+		if i == excludeIdx {
+			continue
+		}
+
+		num, err := strconv.ParseInt(report[i], 10, 64)
+		if err != nil {
+			panic(err)
+		}
+		if last == -1 {
+			last = num
+			continue
+		}
+
+		diff := num - last
+		diffAbs := int64(math.Abs(float64(diff)))
+
+		// Ensuire consistency and difference in range
+		if diffAbs < 1 || diffAbs > 3 || (increasing && diff <= 0) || (decreasing && diff >= 0) {
+			// fmt.Printf("%v is not safe diff=%d increasing=%v decreasing=%v", report, diff, increasing, decreasing)
+			return false
+		}
+
+		if !increasing && !decreasing {
+			if diff > 0 {
+				increasing = true
+			} else if diff < 0 {
+				decreasing = true
+			} else {
+				panic("should not happen")
+			}
+		}
+		last = num
+	}
+	return true
+
+}
+
 func Part2(input string) int {
+	reports := strings.Split(input, "\n")
+	safe := 0
+	for _, report := range reports {
+		if len(report) == 0 {
+			println("empty report")
+		}
+		numbersStr := strings.Split(report, " ")
+		is_safe := false
+		if isSafe(numbersStr, -1) {
+			safe++
+			fmt.Printf("%s is safe\n", report)
+			is_safe = true
+		} else {
+			// fmt.Printf("%s is not safe naturally", report)
+			for i := range numbersStr {
+				if isSafe(numbersStr, i) {
+					// Without idx i the report is safe
+					safe++
+					is_safe = true
+					fmt.Printf("%s is safe after remove\n", report)
+					break
+				}
+			}
+		}
+		if !is_safe {
+			fmt.Printf("%s is not safe\n", report)
+		}
+	}
+	return safe
+}
+
+func Part2Linear(input string) int {
 	reports := strings.Split(input, "\n")
 	safe := 0
 	for _, report := range reports {
@@ -152,26 +224,46 @@ func Part2(input string) int {
 
 			diff := num - last
 			diffAbs := int64(math.Abs(float64(diff)))
+			// log.Printf("%d %d diff=%d inc=%v dec=%v", last, num, diff, increasing, decreasing)
 
 			// Ensuire consistency and difference in range
 			if diffAbs < 1 || diffAbs > 3 || (increasing && diff <= 0) || (decreasing && diff >= 0) {
-				if count > 1 {
-					log.Print("Second error, stoping check for this report")
+				if count > 0 {
 					correct = false
+					count++
 					break
 				}
 				if i == 1 {
 					// If it first element, we can just drop it, and try again
-					if verify(numbersStr[i], numbersStr[i+1], false, false) || verify(numbersStr[i-1], numbersStr[i+1], false, false) {
-						// We can drop i==0
-						log.Print("Dropping i==0 or i==1 when i==1")
+					if verify(numbersStr[i], numbersStr[i+1], false, false) {
+						new_inc, new_dec := calculateIncDec(numbersStr[i], numbersStr[i+1])
+						// Verify that indeed we need to drop the i==0, because we may need to drop the i==1
+						if verify(numbersStr[i+1], numbersStr[i+2], new_inc, new_dec) {
+							// We can drop i==0
+							log.Print("Removed i==0")
+							count++
+							next, err := strconv.ParseInt(numbersStr[i+2], 10, 64)
+							if err != nil {
+								panic("parse next")
+							}
+							// But we need to calculate increasing decreasing
+							increasing, decreasing = new_inc, new_dec
+							// Skip next element, cause we just checked it
+							i += 2
+							last = next
+							continue
+						}
+					}
+					if verify(numbersStr[i-1], numbersStr[i+1], false, false) {
+						// We can drop i==1
+						log.Print("Removed i==1")
 						count++
 						next, err := strconv.ParseInt(numbersStr[i+1], 10, 64)
 						if err != nil {
 							panic("parse next")
 						}
 						// But we need to calculate increasing decreasing
-						increasing, decreasing = calculateIncDec(numbersStr[i], numbersStr[i+1])
+						increasing, decreasing = calculateIncDec(numbersStr[i-1], numbersStr[i+1])
 						// Skip next element, cause we just checked it
 						i++
 						last = next
@@ -183,14 +275,19 @@ func Part2(input string) int {
 					}
 				} else if i == len(numbersStr)-1 {
 					// Last element can just be removed
-					log.Print("Dropping last")
 					count++
 					continue
 				} else if i == 2 {
 					// 0 2
-					if verify(numbersStr[i-1], numbersStr[i+1], increasing, decreasing) {
-						// We can drop current element
-						log.Printf("Dropping %d[%d] when i==2", num, i)
+					if verify(numbersStr[i-1], numbersStr[i], false, false) {
+						// We can drop first element
+						log.Print("Removed i==0")
+						count++
+						increasing, decreasing = calculateIncDec(numbersStr[i-1], numbersStr[i])
+						last = num
+						continue
+					} else if verify(numbersStr[i-1], numbersStr[i+1], increasing, decreasing) {
+						// log.Print("Removed i==2")
 						count++
 						next, err := strconv.ParseInt(numbersStr[i+1], 10, 64)
 						if err != nil {
@@ -200,8 +297,8 @@ func Part2(input string) int {
 						last = next
 						continue
 					} else if verify(numbersStr[i-2], numbersStr[i], false, false) {
-						// We can remove i==1. This can only happen when we 1 and 3 have oposite inequality to 1 and 2. Because otherwise, we would just be out of range
-						log.Printf("dropping %d[%d] when i==2", last, i-1)
+						// We can remove i==1. Monotonicity may change
+						log.Print("Removed i==1")
 						count++
 						last = num
 						increasing, decreasing = calculateIncDec(numbersStr[i-2], numbersStr[i])
@@ -213,8 +310,8 @@ func Part2(input string) int {
 				} else {
 					// i in [3, len-2]
 					if verify(numbersStr[i-1], numbersStr[i+1], increasing, decreasing) {
+						// log.Printf("Removed i==%d", i)
 						// We can drop current element
-						log.Printf("Dropping %d[%d] when i is in the middle", num, i)
 						count++
 						next, err := strconv.ParseInt(numbersStr[i+1], 10, 64)
 						if err != nil {
@@ -242,10 +339,15 @@ func Part2(input string) int {
 
 		}
 		if correct || !correct && count == 1 {
-			log.Printf("Report %s, is safe\n", report)
+			if count == 1 {
+				fmt.Printf("%s is safe after remove\n", report)
+			} else {
+
+				fmt.Printf("%s is safe\n", report)
+			}
 			safe += 1
 		} else {
-			log.Printf("Report %s, is not safe\n", report)
+			fmt.Printf("%s is not safe\n", report)
 		}
 	}
 
